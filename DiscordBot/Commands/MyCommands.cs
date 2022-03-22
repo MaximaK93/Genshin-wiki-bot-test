cbc587f30d841130527dda7d54e44179de16457e
Son Artem <artem23.son@gmail.com>
2022-03-22 13:31:15 +0600

    Add project files.

---
+++ DiscordBot/Commands/MyCommands.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.IO;
+using System.Text;
+using DiscordBot;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DiscordBot.Commands
+{
+    public class MyCommands : ModuleBase<SocketCommandContext>
+    {
+        
+
+        [Command("айди")]
+        public async Task GetMyId()
+        {
+            var user = Context.User;
+            await ReplyAsync($"{user.Mention}, Ваш ID: {user.Id}");            
+        } 
+
+        [Command("время")]
+
+        public async Task GetCurrentTime()
+        {
+            DateTime date = DateTime.Now;
+            await ReplyAsync($"{date}");
+           
+        }
+
+        [Command("ролл")]
+
+        public async Task Roll()
+        {      
+            Random roll = new Random();
+            var user = Context.User;
+            await ReplyAsync(user.Mention + $" Получает число {roll.Next(-1,101)}");
+        }
+    }
+}
+
---
+++ DiscordBot/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DiscordBot
+{
+    public class Bot
+    {
+        
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        static void Main(string[] args)
+            => new Bot().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = "OTUzOTgxODY1NzA5MTUwMjA4.YjMe2A.X0CiAAL6LHF2R8prg_JGKu_TBTI";
+
+            client.Log += clientLog;
+
+            client.MessageReceived += HandleCommandAsync;
+            
+
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage msg)
+        {
+            var message = msg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot)
+            {
+                return;
+            }
+            int argPos = 0;
+            if(message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if(!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition))
+                    await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+
+        private Task clientLog(LogMessage message)
+        {
+            Console.WriteLine(message);
+            return Task.CompletedTask;
+        }
+        
+    }
+}
+    
+
+      
+
+        
+            
+            
+
+           
+
+
+        
+
+       
+        
+        
+
+        
+    
+
+
+
